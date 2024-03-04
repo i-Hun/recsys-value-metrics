76b2c6a2bd7d650dc8a79b2486d85649b1228729
Oleg Nagornyi <i.hun@ya.ru>
2024-03-04 16:51:23 +0400

    gitignore

--- .ipynb_checkpoints/names-checkpoint.py
+++
@@ -1,8 +0,0 @@
-class Columns:
-    """Fixed column names for tables that contain interactions and recommendations."""
-
-    Query = "query_column"
-    Item = "item_column"
-    Score = "score"
-    Weight = "weight"
-    QueryItem = [Query, Item]
